3bb3fa03a21c028188618f79e5ba0823ebddc3f3
Juan <juan.lozada1905@gmail.com>
2023-01-08 13:25:38 -0300

    proyecto completo

--- src/Path/pathControl.js
+++ src/Path/pathControl.js
@@ -1,19 +1,22 @@
-const consults = {}
+
 const { feed, post } = require('../Credentials/function')
 
-consults.get = async (req, res) => {
+const consults = {
 
-    const posts = await feed();
-    res.json(posts);
+    get :  async (req, res) => {
 
-}
-
-consults.post = async (req, res) => {
-
-    const { titulo, url, descripcion } = req.body;
-    await post(titulo, url, descripcion);
-    res.send("El posts se agregó de manera exitosa");
-
+        const posts = await feed();
+        res.json(posts);
+    
+    },
+    
+    post : async (req, res) => {
+    
+        const { titulo, url, descripcion } = req.body;
+        const posts = await post(titulo, url, descripcion);
+        res.json(posts);
+    
+    }
 }
 
 module.exports = consults;
